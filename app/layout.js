64aaaec817a1d2f2683a23ba644e40d4bac7912d
Zaid Ahmad <zaidd250@gmail.com>
2024-07-28 19:51:59 -0600

    add icons

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,25 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
     title: "Lead Flow Manager",
     description: "Track your D2D leads with ease.",
+    icons: {
+        apple: "/apple-touch-icon.png",
+        icon: [
+            { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+            { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+        ],
+        other: [
+            {
+                rel: "mask-icon",
+                url: "/safari-pinned-tab.svg",
+                color: "#5bbad5",
+            },
+        ],
+    },
+    manifest: "/site.webmanifest",
+    themeColor: "#ffffff",
+    msapplication: {
+        tileColor: "#da532c",
+    },
 };
 
 export const viewport = {
